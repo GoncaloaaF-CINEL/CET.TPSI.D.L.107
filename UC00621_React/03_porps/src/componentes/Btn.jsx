a039807f0e485205ca489f2c0c7e26d86dbec835
GoncaloaaF <goncaloaaf@gmail.com>
2026-04-20 12:04:24 +0100

    demo 2 porps

--- UC00621_React/03_porps/src/componentes/Btn.jsx
+++ UC00621_React/03_porps/src/componentes/Btn.jsx
@@ -1,9 +1,19 @@
-function Btn(){
+function Btn({item }){
     return (
         <>
+            <li>
+            <a href={ item.link } target="_blank">
+                <svg
+                    className="button-icon"
+                    role="presentation"
+                    aria-hidden="true"
+                >
+                    <use href={ `/icons.svg#${item.iconSvg}` }></use>
+                </svg>
+                {item.nome}
+            </a>
 
-
-
+            </li>
         </>
     )
 }
--- UC00621_React/03_porps/src/componentes/Social.jsx
+++ UC00621_React/03_porps/src/componentes/Social.jsx
@@ -1,17 +1,16 @@
+import Btn from "./Btn.jsx";
+
 const lista =  [
-    { nome: "GitHub", iconSvg: "github-icon", link: "https://github.com/vitejs/vite" },
-    { nome: "Discord", iconSvg: "discord-icon", link: "https://chat.vite.dev/" },
-    { nome: "X.com", iconSvg: "x-icon", link: "https://x.com/vite_js" },
-    { nome: "Bluesky", iconSvg: "bluesky-icon", link: "https://bsky.app/profile/vite.dev" },
+    {id:1,  nome: "GitHub", iconSvg: "github-icon", link: "https://github.com/vitejs/vite" },
+    {id:2,  nome: "Discord", iconSvg: "discord-icon", link: "https://chat.vite.dev/" },
+    {id:3,  nome: "X.com", iconSvg: "x-icon", link: "https://x.com/vite_js" },
+    {id:4,  nome: "Bluesky", iconSvg: "bluesky-icon", link: "https://bsky.app/profile/vite.dev" },
 ]
-
-
 
 export default function Social({titulo, description, icon}) {
 
     return (
         <>
-
             <div id="social">
                 <svg className="icon" role="presentation" aria-hidden="true">
                     <use href={ `icons.svg#${ icon }` }></use>
@@ -20,18 +19,13 @@ export default function Social({titulo, description, icon}) {
                 <p>{ description }</p>
                 <ul>
 
-                    <li>
-                        <a href="https://github.com/vitejs/vite" target="_blank">
-                            <svg
-                                className="button-icon"
-                                role="presentation"
-                                aria-hidden="true"
-                            >
-                                <use href="/icons.svg#github-icon"></use>
-                            </svg>
-                            GitHub
-                        </a>
-                    </li>
+                    {lista.map(item => (
+                        <Btn
+                            key={ item.id }
+                            item={ item }
+                        />
+                    ))
+                    }
 
                 </ul>
             </div>
